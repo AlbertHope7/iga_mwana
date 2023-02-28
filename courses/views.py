4aac4b9ae1b5834de68e1702261f098600c5365e
AlbertHope7 <byiringiroalbert20@gmail.com>
2023-02-28 16:12:07 +0200

    reordering modules and their contents

--- courses/views.py
+++ courses/views.py
@@ -170,8 +170,20 @@ class ModuleContentListView(TemplateResponseMixin, View):
         return self.render_to_response({"module": module})
 
 
-# class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
-#     def post(self, request):
-#         for id, order in self.request__json.items():
-#             Module.objects.filter(id=id, course__owner=request.user).update(order=order)
-#         return self.render_json_response({"saved": "OK"})
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        print("watch out")
+
+        return self.render_json_response({"saved": "OK"})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request__json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(
+                order=order
+            )
+        print("watch out")
+        return self.render_json_response({"saved": "OK"})
